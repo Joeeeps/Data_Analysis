4320cc7d40d68135ce9b8e8fb512fa64e40b5dc5
Joe PS <61272205+Joeeeps@users.noreply.github.com>
2024-01-19 04:24:20 +0000

    Add files via upload

---
+++ ONS_test/00_Assignment/build_reference.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/joe/miniconda3/bowtie2-build /home/joe/miniconda3/bowtie2-build /home/joe/Desktop/Coding/R_analysis/ONS_test/00_Assignment/reference/reference.fasta reference_index/reference.fasta reference_index
+
